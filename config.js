ade2f45c0c00e0fc2d6d83a9412e866a45ba8545
DanielWent <109043916+DanielWent@users.noreply.github.com>
2026-02-15 22:09:05 +0000

    Update config.js

--- config.js
+++ config.js
@@ -17,8 +17,10 @@ config.gsheets_key_path = config.output_dir + "withings2gsheets-service-account.
 config.sqlite3_output_path = config.output_dir + "withings_data.db";
 config.csv_output_path = config.data_dir + "withings_data.csv";
 
+// User Height in Meters (185 cm)
 config.height = 1.85; 
 
+// Metric Type IDs for Withings Body Scan
 config.metrics = {
     "1": "Weight (kg)",
     "6": "Body Fat (%)",
