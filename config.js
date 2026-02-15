f44884fe3910650864741fac285371bb1eef830a
DanielWent <109043916+DanielWent@users.noreply.github.com>
2026-02-15 21:33:58 +0000

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ config.gsheets_key_path = config.output_dir + "withings2gsheets-service-account.
 config.sqlite3_output_path = config.output_dir + "withings_data.db";
 config.csv_output_path = config.data_dir + "withings_data.csv";
 
-// User Height in Meters (185 cm)
+// Your Height in Meters
 config.height = 1.85; 
 
 // Metric Type IDs to Name mapping
@@ -30,7 +30,7 @@ config.metrics = {
     "9": "Diastolic Blood Pressure (mmHg)",
     "10": "Systolic Blood Pressure (mmHg)",
     "11": "Heart Pulse (bpm)",
-    "12": "Visceral Fat Rating", // Likely incorrect, but kept for now.
+    "125": "Visceral Fat Rating", // Updated to correct Body Scan ID
     "54": "SP02 (%)",
     "71": "Body Temperature (celsius)",
     "73": "Skin Temperature (celsius)",
@@ -44,7 +44,7 @@ config.metrics = {
     "158": "Nerve Health Score"
 };
 
-// COMMENTED OUT to force fetching ALL metrics for discovery
-// config.metricList = "1,4,5,6,8,9,10,11,12,54,71,73,76,77,88,91,123,130,155,158";
+// Request string (including 125)
+config.metricList = "1,4,5,6,8,9,10,11,54,71,73,76,77,88,91,123,125,130,155,158";
 
 module.exports = config;
